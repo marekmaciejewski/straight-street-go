4270df78a604c130a379bfaeedba338bd177032b
Marek Maciejewski <mmaciejewski@altimetrik.com>
2026-08-11 12:18:52 +0200

    init

---
+++ src/main/java/pl/mm/straightstreetgo/StraightStreetGoApplication.java
@@ -0,0 +1,13 @@
+package pl.mm.straightstreetgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StraightStreetGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StraightStreetGoApplication.class, args);
+    }
+
+}
